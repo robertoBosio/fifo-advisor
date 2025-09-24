7f4a86bc9c5fad7d4479d6c0f9c3ff32bb65a890
Stefan Abi-Karam <stefanabikaram@gmail.com>
2025-09-23 20:49:37 -0400

    updates

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # FIFOAdvisor
+
+FIFOAdvisor is a framework that optimizes FIFO depths in HHigh Level Synthesis designs, specifically targeting Vitis HLS. It can search for optimal FIFO depths to minimize latency and resource usage, and also size them to avoid deadlocks.
+
+## Quick Start
+
+Install with `uv`:
+
+```bash
+uv install git+https://github.com/sharc-lab/fifo-advisor.git
+```
+
+Install with `pip`:
+
+```bash
+pip install git+https://github.com/sharc-lab/fifo-advisor.git
+```
+
+Run FIFOAdvisor:
+
+```bash
+fifo-opt --help
+```
--- fifo_opt/main.py
+++ fifo_opt/main.py
@@ -5,22 +5,31 @@ from fifo_opt.opt_env import LSEnv
 from fifo_opt.solvers import RandomSearchOptimizer
 
 
-def main(args):
-    solution_dir: Path = args.solution_dir
-    sim_env = LSEnv(solution_dir)
-    optimizer = RandomSearchOptimizer(sim_env)
-    optimizer.solve()
-
-
-if __name__ == "__main__":
+def build_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
         prog="fifo_opt",
         description=(
             "A tool for optimizing FIFOs depths in a high-level synthesis design using"
             " different optimization algorithms and fast co-simulation."
         ),
-        usage="python fifo_opt.py solution_dir",
+        usage="fifo_opt <solution_dir>",
     )
     parser.add_argument("solution_dir", type=Path)
+    return parser
+
+
+def main(args: argparse.Namespace) -> None:
+    solution_dir: Path = args.solution_dir
+    sim_env = LSEnv(solution_dir)
+    optimizer = RandomSearchOptimizer(sim_env)
+    optimizer.solve()
+
+
+def cli() -> None:
+    parser = build_parser()
     args = parser.parse_args()
     main(args)
+
+
+if __name__ == "__main__":
+    cli()
